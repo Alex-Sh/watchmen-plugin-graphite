d89d88423e796d1af985f33b3471025d45028c34
Alexander Shovdra <ashovdra@betterdecisionmedia.com>
2017-05-23 08:44:00 +0300

    Update metric values

--- index.js
+++ index.js
@@ -52,18 +52,21 @@ function sendEvent (body) {
  * Watchmen event handlers
  */
 var eventHandlers = {
+
   onFailedCheck: function (service, data) {
-    // Write failureInterval in ms. to calculate clear failure time
+    // Send new outage indication with failureInterval in ms. for full downtime calculations
     graphite.push('watchmen.' + filterName(service.name) + '.failedCheck', service.failureInterval);
   },
 
   onServiceOk: function (service, data) {
+    // Send success check load time
     graphite.push('watchmen.' + filterName(service.name) + '.serviceOk', data.elapsedTime);
   },
 
   onNewOutage: function (service, outage) {
-    // Send just an event indicator, 1
-    graphite.push('watchmen.' + filterName(service.name) + '.newOutage', 1);
+    // Send new outage indication with real start date calculated from failureInterval and failuresToBeOutage
+    var failuresToBeOutage = isNaN(service.failuresToBeOutage) ? 1 : service.failuresToBeOutage;
+    graphite.push('watchmen.' + filterName(service.name) + '.newOutage', service.failureInterval * failuresToBeOutage);
     sendEvent({
       what: 'OUTAGE',
       tags: 'watchmen ' + service.name + ' outage',
@@ -75,7 +78,7 @@ var eventHandlers = {
   },
 
   onServiceBack: function (service, lastOutage) {
-    // Send downtime duration in ms.
+    // Send downtime duration in ms. (from latest outage)
     graphite.push('watchmen.' + filterName(service.name) + '.serviceBack', new Date().getTime() - lastOutage.timestamp);
 
     var duration = Math.round((new Date().getTime() - lastOutage.timestamp) / 1000);
